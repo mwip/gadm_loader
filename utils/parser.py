aba60c8f82bbda3c659e5a25e65bf19c0d40a57f
Matthias Weigand <matthias.weigand@protonmail.com>
2021-12-21 17:32:58 +0100

    Fix: include countries with spaces in names

--- utils/parser.py
+++ utils/parser.py
@@ -24,41 +24,46 @@
 import urllib.request, urllib.error, urllib.parse
 import re
 
+
 def parse_gadm_countries():
-    url = 'https://gadm.org/download_country_v3.html'
+    url = "https://gadm.org/download_country_v3.html"
     try:
-        response = urllib.request.urlopen(url, timeout = 30)
+        response = urllib.request.urlopen(url, timeout=30)
         content = response.read()
     except urllib.error.HTTPError as e:
         print(e.msg, ":", url)
     except urllib.error.URLError as e:
         print(e.reason, ":", url)
-        
+
     # from the http content, we want to find all countries listed inside <option value> tags
-    values = re.findall(r"<option value=\"\w*\">\w+</option>", str(content))
-    
+    values = re.findall(r"<option value=\"[\w\s]*\">[\w|\s]+</option>", str(content))
+
     # extract country codes and names from the values
     codes = [re.search(r"[A-Z]{3}", str(x)).group() for x in list(values)]
     names = [re.search(r"(?<=>)[A-Za-z ]+(?=<)", str(x)).group() for x in list(values)]
-    
+
     # zip country codes and values into a dictionary
     out = dict(zip(names, codes))
     # add the world
-    out['Entire World'] = "WORLD"
-    
+    out["Entire World"] = "WORLD"
+
     return out
 
 
 # construct url
-def get_url(self):
-    if self.country_code == "WORLD":
-        if self.format_gpkg:
+def get_url(gadmloader):
+    if gadmloader.country_code == "WORLD":
+        if gadmloader.format_gpkg:
             url = "https://biogeo.ucdavis.edu/data/gadm3.6/gadm36_gpkg.zip"
         else:
             url = "https://biogeo.ucdavis.edu/data/gadm3.6/gadm36_shp.zip"
     else:
-        url = "https://biogeo.ucdavis.edu/data/gadm3.6/" + \
-            ("gpkg" if self.format_gpkg else "shp") + \
-            "/gadm36_" + self.country_code + "_" + \
-            ("gpkg.zip" if self.format_gpkg else "shp.zip")
+        url = (
+            "https://biogeo.ucdavis.edu/data/gadm3.6/"
+            + ("gpkg" if gadmloader.format_gpkg else "shp")
+            + "/gadm36_"
+            + gadmloader.country_code
+            + "_"
+            + ("gpkg.zip" if gadmloader.format_gpkg else "shp.zip")
+        )
     return url
